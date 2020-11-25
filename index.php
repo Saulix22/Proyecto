9c2872a3aa1a258477053b31067506e848e0d459
Óscar Miranda <oscarmiranda3615@gmail.com>
2020-11-24 17:49:23 -0700

    Usar Bootstrap

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 require 'partials/db.php';
-
-if (isset($_POST['user']) && isset($_POST['password'])) {
-	echo 'Inicio de sesión';	
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-		<link rel="stylesheet" href="css/main.css">
+		<title></title>
+		
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	</head>
 	<body>
-		<nav class="navbar">Logo</nav>
+		<?php require 'components/navbar.php'; ?>
 
 		<div class="container">
 			<form class="loginForm" action="index.php" method="post">
@@ -34,5 +32,8 @@ if (isset($_POST['user']) && isset($_POST['password'])) {
 				</div>
 			</div>
 		</div>
+
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	</body>
 </html>
