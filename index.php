c6063fd4f5e7ec3455c158ec4a6a11b5057453b7
Saul <nadamasaul@gmail.com>
2020-11-24 19:06:36 -0700

    Login redesiged

--- index.php
+++ index.php
@@ -10,28 +10,32 @@ require 'partials/db.php';
 
 		<title></title>
 		
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<link rel="styleshe
+		et" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<link rel="stylesheet" href="CSS/style.css">
+
 	</head>
 	<body>
-		<?php require 'components/navbar.php'; ?>
 
-		<div class="container">
+		
+		<div class="container" id="login">
 			<form class="loginForm" action="index.php" method="post">
-				<h2 class="formTitle">Iniciar sesión</h2>
+				<h1 class="formTitle">Iniciar sesión</h1>
 
 				<div class="inputWrapper">
-					<label for="user">Usuario:</label>
-					<input class="formField" type="text" name="user" required>
+					<label for="user">Correo:</label>
+					<input class="formField" type="text" name="user" placeholder="al@alumnos.uacj.mx" required>
 				</div>
 				<div class="inputWrapper">
 					<label for="password">Contraseña:</label>
 					<input class="formField" type="password" name="password" required>
 				</div>
 				<div class="inputWrapper">
-					<input class="formButton" type="submit" value="Iniciar sesión">
+					<input class="formButton" type="submit" value="Entrar">
 				</div>
 			</div>
 		</div>
+		
 
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
